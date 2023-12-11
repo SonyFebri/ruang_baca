14ebb2158ea8d68ace12a2bd9ad97f9dcc6f9168
Sony Febri Hari Wibowo <sonywib04@gmail.com>
2023-12-11 21:50:40 +0700

    add logo

--- app/views/partials/header.view.php
+++ app/views/partials/header.view.php
@@ -2,7 +2,6 @@
 .user-profile {
     position: relative;
     display: inline-block;
-    /* Menjadikan kontainer sejajar dengan elemen sekitarnya */
 }
 
 .user-info {
@@ -11,33 +10,42 @@
 }
 
 .user-name {
-    margin-top: 10px;
-    margin-right: 50px;
-    /* Jarak antara nama & gambar */
+    margin-top: 1px;
+    margin-right: 30px;
+    /* Mengurangi jarak antara nama dan gambar */
+}
+
+.cart-logo {
+    width: 20px;
+    /* Sesuaikan dengan ukuran yang diinginkan */
+    height: 20px;
+    /* Sesuaikan dengan ukuran yang diinginkan */
+    margin-right: 10px;
 }
 
 .user-profile img {
     width: 40px;
-    /* Sesuaikan dengan ukuran yang diinginkan */
     height: 40px;
-    /* Sesuaikan dengan ukuran yang diinginkan */
     border-radius: 40%;
-    position: absolute;
-    top: 5px;
-    /* Sesuaikan dengan jarak dari atas yang diinginkan */
-    right: 5px;
-    /* Sesuaikan dengan jarak dari kanan yang diinginkan */
     margin-right: 10px;
-    /* Tambahkan margin-right sesuai kebutuhan */
+}
+
+.logo-img {
+    width: 70%;
+    height: auto;
+    margin-left: 20px;
 }
 </style>
+
 <nav class="navbar">
-    <div class="logo">
-        <img src="logo.png" alt="Logo">
-        RUANG BACA
+    <div class="logo-container">
+        <img src="<?php echo App::get("root_uri") . "/public/logo/Logo Ruang Baca.png" ?>" alt="Logo" class="logo-img">
     </div>
+
     <div class="user-profile">
         <div class="user-info">
+            <img src="<?php echo App::get("root_uri") . "/public/logo/cart.png" ?>" alt="Keranjang Belanja"
+                class="cart-logo">
             <span class="user-name">
                 <?php echo Session::getInstance()->get('nama') ?>
             </span>
@@ -46,5 +54,4 @@
             <span class="badge">3</span>
         </div>
     </div>
-
 </nav>
